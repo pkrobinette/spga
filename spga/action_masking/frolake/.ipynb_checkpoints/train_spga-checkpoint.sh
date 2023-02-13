a4c58f7f7576758432059b1e911669541c7ebdd0
pkrobinette <69876483+pkrobinette@users.noreply.github.com>
2023-02-12 22:05:09 -0600

    Delete train_spga-checkpoint.sh

--- spga/action_masking/frolake/.ipynb_checkpoints/train_spga-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-python train_ga_frolake_amask.py --num_trials 5 --map 32x32 --max_steps 500 --num_eval_eps 500
-python train_ga_frolake_amask.py --num_trials 5 --map 16x16 --max_steps 500 --num_eval_eps 500
-python train_ga_frolake_amask.py --num_trials 5 --map 8x8 --max_steps 500 --num_eval_eps 500
